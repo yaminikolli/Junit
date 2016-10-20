b1c4694f3b50b0112d60378e104614123f10b072
kyamini <yamini.kolli@incubesol.com>
2016-10-20 14:44:53 +0530

    adding files

---
+++ hellotest.java
@@ -0,0 +1,18 @@
+package hello;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class hellotest {
+	@Test
+	   public void testString() {
+	      String str= "Junit, Hello world!" ;
+	      assertEquals("Junit, Hello world!",str);
+	      System.out.println("Pass");
+	   }
+}
+
+
+
+
+	
